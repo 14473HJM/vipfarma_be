f8afd0beff9cb20018e4b85566391da7f9efed84
Hernán Jesús Morais <104473@tecnicatura.frc.utn.edu.ar>
2022-09-03 12:25:58 -0300

    Rename main method and change to web app

--- src/main/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/Application.java
+++ src/main/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VipFarmaBackEndApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VipFarmaBackEndApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/ApplicationTests.java
+++ src/test/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VipFarmaBackEndApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
