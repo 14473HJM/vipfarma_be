88d101593868e185993af0dfe000428e790bcc4e
Hernán Jesús Morais <104473@tecnicatura.frc.utn.edu.ar>
2022-09-03 12:10:24 -0300

    SpringBoot initial scaffolding

---
+++ src/main/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/VipFarmaBackEndApplication.java
@@ -0,0 +1,13 @@
+package ar.com.utn.frc.msi.tpi.vipFarmaBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipFarmaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipFarmaBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/VipFarmaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.utn.frc.msi.tpi.vipFarmaBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VipFarmaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
