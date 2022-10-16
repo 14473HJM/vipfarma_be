957d1cc50025a25178149049b85cf247b022409f
Hernán Jesús Morais <104473@tecnicatura.frc.utn.edu.ar>
2022-10-16 18:35:41 -0300

    Stock Capabilities

---
+++ src/main/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/entity/StockEntity.java
@@ -0,0 +1,34 @@
+package ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.entity;
+
+import ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.model.catalog.Product;
+import ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.model.stock.Locker;
+import ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.model.stock.StockStatus;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity(name = "stocks")
+public class StockEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "productId", referencedColumnName = "id")
+    private ProductEntity productId;
+
+    private LocalDate createdDate;
+    private LocalDate endDate;
+    private LocalDate dueDate;
+    private Integer initialStock;
+    private Integer availableStock;
+
+    @OneToOne
+    @JoinColumn(name = "lockerId", referencedColumnName = "id")
+    private LockerEntity lockerId;
+
+    private StockStatus stockStatus;
+}
