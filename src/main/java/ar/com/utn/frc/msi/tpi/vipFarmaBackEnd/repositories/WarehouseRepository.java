60c0de2c8ab7f9953a61c373801149fa08ec63a5
Hernán Jesús Morais <104473@tecnicatura.frc.utn.edu.ar>
2022-10-16 12:07:48 -0300

    Update WarehouseRepository.java

--- src/main/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/repositories/WarehouseRepository.java
+++ src/main/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/repositories/WarehouseRepository.java
@@ -2,6 +2,8 @@ package ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.repositories;
 
 import ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.entity.WarehouseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WarehouseRepository extends JpaRepository<WarehouseEntity, Long> {
 }
