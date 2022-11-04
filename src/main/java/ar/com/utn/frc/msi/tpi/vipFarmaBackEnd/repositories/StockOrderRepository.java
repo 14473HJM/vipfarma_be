1b9745f14ebb808bcb0328b496d84937e0af02f0
14473HJM <104473@tecnicatura.frc.utn.edu.ar>
2022-11-03 22:52:14 -0300

    new capabilities

--- src/main/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/repositories/StockOrderRepository.java
+++ src/main/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/repositories/StockOrderRepository.java
@@ -5,10 +5,16 @@ import ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.entity.WarehouseEntity;
 import ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.model.stock.StockOrderStatus;
 import ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.model.stock.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface StockOrderRepository extends JpaRepository<StockOrderEntity, Long> {
 
     List<StockOrderEntity> getByStockOrderStatusAndWarehouseId(StockOrderStatus stockOrderStatus, WarehouseEntity warehouse);
+    @Query("SELECT S " +
+            "FROM stockOrders S " +
+            "WHERE S.warehouseId.branchOffice.id = :branchOfficeId " +
+            "AND S.stockOrderStatus = :stockOrderStatus ")
+    List<StockOrderEntity> getByStockOrderStatusAndBranchOffice(StockOrderStatus stockOrderStatus, Long branchOfficeId);
 }
