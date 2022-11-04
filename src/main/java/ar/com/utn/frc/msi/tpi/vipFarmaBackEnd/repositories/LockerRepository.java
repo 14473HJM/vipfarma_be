13a6df462be840a76d3bf224dc6607ac8a173aaf
14473HJM <104473@tecnicatura.frc.utn.edu.ar>
2022-11-03 23:19:09 -0300

    Lockers

--- src/main/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/repositories/LockerRepository.java
+++ src/main/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/repositories/LockerRepository.java
@@ -1,9 +1,21 @@
 package ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.repositories;
 
 import ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.entity.LockerEntity;
+import ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.model.catalog.Product;
+import ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.model.stock.Locker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LockerRepository extends JpaRepository<LockerEntity, Long> {
+
+    List<Locker> getAllByProductId(Long productId);
+
+    @Query("SELECT L FROM lockers L " +
+            "WHERE L.product.id = :productId " +
+            "AND (L.stockCapacity - L.occupiedCapacity) > :availability")
+    List<Locker> getAllByProductAndAvailability(Long productId, Integer availability);
 }
--- src/main/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/services/LockerService.java
+++ src/main/java/ar/com/utn/frc/msi/tpi/vipFarmaBackEnd/services/LockerService.java
@@ -1,7 +1,15 @@
 package ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.services;
 
 import ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.entity.LockerEntity;
+import ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.model.catalog.Product;
 import ar.com.utn.frc.msi.tpi.vipFarmaBackEnd.model.stock.Locker;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LockerService extends BaseModelService<Locker, LockerEntity> {
+
+    List<Locker> getAllByProduct(Long productId);
+
+    List<Locker> getAllByProductAndAvailability(Long productId, Integer availability);
 }
